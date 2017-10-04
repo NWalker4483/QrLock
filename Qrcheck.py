84a52ac0b6a9f201054fc89a20c7a1f7d31803f0
Nile Walker <niwal7@morgan.edu>
2017-10-04 15:15:27 -0400

    Initial File Upload

---
+++ Qrcheck.py
@@ -0,0 +1,46 @@
+import zbar
+import cv2
+import pymysql
+import imutils
+from PIL import Image
+import re
+db=pymysql.connect(host="sql9.freemysqlhosting.net", user="sql9197760", passwd="hwIDaFzRZA", db="sql9197760")
+ex=db.cursor(pymysql.cursors.DictCursor)
+cam=cv2.VideoCapture(0)
+found=False
+chars = re.escape(string.punctuation)
+cv2.namedWindow("base-image", cv2.WINDOW_AUTOSIZE) 
+cv2.startWindowThread()
+def CenterText(text,_img,_color):
+    #Calculates the center of the screen 
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    textsize = cv2.getTextSize(text, font, 1, 2)[0]
+    testX = (_img.shape[1] - textsize[0]) / 2
+    testY = (_img.shape[0] + textsize[1]) / 2
+    cv2.putText(_img,text, (int(testX),int(testY)), font, 1,color=_color,thickness=6)
+
+def taek(found):
+    (_,image) = cam.read() 
+    image = imutils.resize(image, width=400) 
+    nimage = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    nimage = Image.fromarray(nimage)
+    scanner = zbar.Scanner()
+    results = scanner.scan(nimage)
+    for result in results:
+        if len(results) != 0:
+            cv2.line(image, result.position[0],result.position[1], (0, 0, 255), 4)
+            if ex.execute("SELECT name FROM allow_ids WHERE passkey = '{0}';".format(re.sub(r'['+chars+']', '',str(result.data.decode("utf-8"))[:10]))):
+                name=ex.fetchall()[0]['name']
+                #Centers the text
+                CenterText("Welcome, {0}".format(name),image,(0,255,0))
+            else:
+                CenterText("ACCESS DENIED",image,(0,0,255))
+    return image,found
+while True and found == False:
+    key = cv2.waitKey(1) & 0xFF
+    img,found=taek(found)
+    cv2.imshow("base-image",img)
+    if key == ord("q"):
+        break
+cam.release()
+#
